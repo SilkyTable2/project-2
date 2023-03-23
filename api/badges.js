178a19fb1e411806c62c8986d1be61ed29e5a1e7
Paul Simkonis <122229125+SpruceWoodTree@users.noreply.github.com>
2023-03-23 12:25:03 -0400

    added json data + emptied main file
    added json data for the cards, emptied out the sample code for our own.

---
+++ api/badges.js
@@ -0,0 +1,40 @@
+
+export default async function handler(request, res) {
+
+    const badges = [
+        {
+            "heading": "Technology & Information",
+            "image": "https://external-preview.redd.it/1MuiO5_KW_8rfN3AEGdRxU-p1VEcaQi4bdLb5rBvtjg.png?format=pjpg&auto=webp&s=83247025c60f25bb81bb989c376c58bb9613cd4c",
+            "creator": "Sussus Amogus",
+            "title": "APA Style Citations: Introduction",
+            "color": "#3399FF"
+        },
+        {
+            "heading": "Education",
+            "image": "https://external-preview.redd.it/1MuiO5_KW_8rfN3AEGdRxU-p1VEcaQi4bdLb5rBvtjg.png?format=pjpg&auto=webp&s=83247025c60f25bb81bb989c376c58bb9613cd4c",
+            "creator": "Cassue Rosas-Carson",
+            "title": "IST Career Resources",
+            "color": "#f2dc23"
+        },
+        {
+            "heading": "Professional Skills",
+            "image": "https://external-preview.redd.it/1MuiO5_KW_8rfN3AEGdRxU-p1VEcaQi4bdLb5rBvtjg.png?format=pjpg&auto=webp&s=83247025c60f25bb81bb989c376c58bb9613cd4c",
+            "creator": "Rita Griffith",
+            "title": "IST Careers - Meet a Coach level 2",
+            "color": "#800080"
+        },
+        {
+            "heading": "Agriculture & Natural Resources",
+            "image": "https://external-preview.redd.it/1MuiO5_KW_8rfN3AEGdRxU-p1VEcaQi4bdLb5rBvtjg.png?format=pjpg&auto=webp&s=83247025c60f25bb81bb989c376c58bb9613cd4c",
+            "creator": "Allain Daigle",
+            "title": "[test] Treat Yourself",
+            "color": "#008000"
+        }
+    ];
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.json(badges);
+}
