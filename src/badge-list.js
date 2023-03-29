07abbf8833e2478ac9229eb4dec87cb9bbddfd3b
Paul Simkonis <122229125+SpruceWoodTree@users.noreply.github.com>
2023-03-28 21:26:21 -0400

    did some stuff

--- src/badge-list.js
+++ src/badge-list.js
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import "./project-2.js";
+// import "@lrnwebcomponents/simple-icon/simple-icon.js";
+// import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
 class Badgelist extends LitElement {
   static properties = {
--- src/card-temp.js
+++ src/card-temp.js
@@ -1,6 +1,5 @@
 import { LitElement, html, css } from 'lit';
 
-
 class CardTemp extends LitElement {
   static properties = {
     header: { type: String },
